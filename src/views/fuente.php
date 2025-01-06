b1e877a61e3df999d24a233fff76993626c61dc6
SoyNarval <ignaciopelayo@hotmail.es>
2025-01-06 12:08:46 +0100

    contacto

--- src/views/fuente.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $titulo = "Fuente";
-?>
-<h2 class="titulo"><?php echo $titulo; ?></h2>
-
-<div class="about">
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur earum, recusandae minus dignissimos at iusto quo nihil soluta reprehenderit repudiandae ducimus porro aliquam dolor sit aperiam, eaque quis cumque illum.</p>
-</div>
